14b24a4e37ccf502816ecc1edf22d831c6a9c9a9
rspijkers <rik.spijkers@gmail.com>
2021-04-01 11:58:12 +0200

    first version of X candidate creator
    add validation plots to X cand creator
    
    remove collision table from Xtask
    
    some fixes, and added a whole bunch of histo's

--- Analysis/Tasks/PWGHF/taskX.cxx
+++ Analysis/Tasks/PWGHF/taskX.cxx
@@ -21,6 +21,7 @@
 
 using namespace o2;
 using namespace o2::framework;
+using namespace o2::aod::hf_cand_prong3;
 using namespace o2::framework::expressions;
 using namespace o2::aod::hf_cand_prong2;
 
@@ -36,60 +37,120 @@ void customize(std::vector<o2::framework::ConfigParamSpec>& workflowOptions)
 struct TaskX {
   HistogramRegistry registry{
     "registry",
-    {{"hMassJpsi", "2-prong candidates;inv. mass (e+ e-) (GeV/#it{c}^{2});entries", {HistType::kTH1F, {{500, 0., 5.}}}},
-     {"hPtCand", "X candidates;candidate #it{p}_{T} (GeV/#it{c});entries", {HistType::kTH1F, {{100, 0., 10.}}}}}};
+    {{"hMass", "3-prong candidates;inv. mass (J/#psi #pi+ #pi-) (GeV/#it{c}^{2});entries", {HistType::kTH1F, {{500, 0., 10.}}}},
+     {"hPtProng0", "3-prong candidates;prong 0 #it{p}_{T} (GeV/#it{c});entries", {HistType::kTH1F, {{100, 0., 10.}}}},
+     {"hPtProng1", "3-prong candidates;prong 1 #it{p}_{T} (GeV/#it{c});entries", {HistType::kTH1F, {{100, 0., 10.}}}},
+     {"hPtProng2", "3-prong candidates;prong 1 #it{p}_{T} (GeV/#it{c});entries", {HistType::kTH1F, {{100, 0., 10.}}}},
+     {"hdeclength", "3-prong candidates;decay length (cm);entries", {HistType::kTH1F, {{200, 0., 2.}}}},
+     {"hdeclengthxy", "3-prong candidates;decay length xy (cm);entries", {HistType::kTH1F, {{200, 0., 2.}}}},
+     {"hd0Prong0", "3-prong candidates;prong 0 DCAxy to prim. vertex (cm);entries", {HistType::kTH1F, {{100, -1., 1.}}}},
+     {"hd0Prong1", "3-prong candidates;prong 1 DCAxy to prim. vertex (cm);entries", {HistType::kTH1F, {{100, -1., 1.}}}},
+     {"hd0Prong2", "3-prong candidates;prong 1 DCAxy to prim. vertex (cm);entries", {HistType::kTH1F, {{100, -1., 1.}}}},
+     {"hCPA", "3-prong candidates;cosine of pointing angle;entries", {HistType::kTH1F, {{110, -1.1, 1.1}}}},
+     {"hEta", "3-prong candidates;candidate #it{#eta};entries", {HistType::kTH1F, {{100, -2., 2.}}}},
+     {"hImpParErr", "3-prong candidates;impact parameter error (cm);entries", {HistType::kTH1F, {{100, -1., 1.}}}},
+     {"hDecLenErr", "3-prong candidates;decay length error (cm);entries", {HistType::kTH1F, {{100, 0., 1.}}}},
+     {"hDecLenXYErr", "3-prong candidates;decay length xy error (cm);entries", {HistType::kTH1F, {{100, 0., 1.}}}},
+     {"hPtCand", "3-prong candidates;candidate #it{p}_{T} (GeV/#it{c});entries", {HistType::kTH1F, {{100, 0., 10.}}}}}};
 
-  Configurable<int> selectionFlagJpsi{"selectionFlagJpsi", 1, "Selection Flag for Jpsi"};
+  // Configurable<int> selectionFlagX{"selectionFlagX", 1, "Selection Flag for X"};
   Configurable<double> cutYCandMax{"cutYCandMax", -1., "max. cand. rapidity"};
 
-  Filter filterSelectCandidates = (aod::hf_selcandidate_jpsi::isSelJpsiToEE >= selectionFlagJpsi);
+  // Filter filterSelectCandidates = (aod::hf_selcandidate_x::isSelXToJpsiPiPi >= selectionFlagX);
 
-  /// aod::BigTracks is not soa::Filtered, should be added when filters are added
-  void process(aod::Collision const&, aod::BigTracks const& tracks, soa::Filtered<soa::Join<aod::HfCandProng2, aod::HFSelJpsiToEECandidate>> const& candidates)
+  // X candidates are not soa::Filtered, should be added when candidate selector is added
+  void process(aod::HfCandX const& candidates)
   {
     for (auto& candidate : candidates) {
-      if (!(candidate.hfflag() & 1 << DecayType::JpsiToEE)) {
-        continue;
-      }
-      if (cutYCandMax >= 0. && std::abs(YJpsi(candidate)) > cutYCandMax) {
-        continue;
-      }
-      registry.fill(HIST("hMassJpsi"), InvMassJpsiToEE(candidate));
+      // TODO: fix hfflag for xToJpsiPiPi
+      // if (!(candidate.hfflag() & 1 << XToJpsiPiPi)) {
+      //   continue;
+      // }
+      // TODO: add pdgcode 9920443 so we can calculate YX in HFSecondaryVertex
+      // if (cutYCandMax >= 0. && std::abs(YX(candidate)) > cutYCandMax) {
+      //   continue;
+      // }
 
-      int index0jpsi = candidate.index0Id();
-      int index1jpsi = candidate.index1Id();
-      for (auto& track1 : tracks) {
-        int signTrack1 = track1.sign();
-        int indexTrack1 = track1.globalIndex();
-        if (signTrack1 > 0) {
-          if (indexTrack1 == index0jpsi) {
-            continue;
-          }
-        } else if (indexTrack1 == index1jpsi) {
+      registry.fill(HIST("hMass"), InvMassXToJpsiPiPi(candidate));
+      registry.fill(HIST("hPtCand"), candidate.pt());
+      registry.fill(HIST("hPtProng0"), candidate.ptProng0());
+      registry.fill(HIST("hPtProng1"), candidate.ptProng1());
+      registry.fill(HIST("hPtProng2"), candidate.ptProng2());
+      registry.fill(HIST("hdeclength"), candidate.decayLength());
+      registry.fill(HIST("hdeclengthxy"), candidate.decayLengthXY());
+      registry.fill(HIST("hd0Prong0"), candidate.impactParameter0());
+      registry.fill(HIST("hd0Prong1"), candidate.impactParameter1());
+      registry.fill(HIST("hd0Prong2"), candidate.impactParameter2());
+      registry.fill(HIST("hCPA"), candidate.cpa());
+      registry.fill(HIST("hEta"), candidate.eta());
+      registry.fill(HIST("hImpParErr"), candidate.errorImpactParameter0());
+      registry.fill(HIST("hImpParErr"), candidate.errorImpactParameter1());
+      registry.fill(HIST("hDecLenErr"), candidate.errorDecayLength());
+      registry.fill(HIST("hDecLenXYErr"), candidate.errorDecayLengthXY());
+    } // candidate loop
+  }   // process
+};    // struct
+
+struct TaskXMC {
+  HistogramRegistry registry{
+    "registry",
+    {{"hPtRecSig", "3-prong candidates (rec. matched);#it{p}_{T} (GeV/#it{c});entries", {HistType::kTH1F, {{100, 0., 10.}}}},
+     {"hPtRecBg", "3-prong candidates (rec. unmatched);#it{p}_{T} (GeV/#it{c});entries", {HistType::kTH1F, {{100, 0., 10.}}}},
+     {"hPtGen", "3-prong candidates (gen. matched);#it{p}_{T} (GeV/#it{c});entries", {HistType::kTH1F, {{100, 0., 10.}}}},
+     {"hCPARecSig", "3-prong candidates (rec. matched);cosine of pointing angle;entries", {HistType::kTH1F, {{110, -1.1, 1.1}}}},
+     {"hCPARecBg", "3-prong candidates (rec. unmatched);cosine of pointing angle;entries", {HistType::kTH1F, {{110, -1.1, 1.1}}}},
+     {"hEtaRecSig", "3-prong candidates (rec. matched);#it{#eta};entries", {HistType::kTH1F, {{100, -2., 2.}}}},
+     {"hEtaRecBg", "3-prong candidates (rec. unmatched);#it{#eta};entries", {HistType::kTH1F, {{100, -2., 2.}}}},
+     {"hEtaGen", "3-prong candidates (gen. matched);#it{#eta};entries", {HistType::kTH1F, {{100, -2., 2.}}}}}};
+
+  // Configurable<int> selectionFlagX{"selectionFlagX", 1, "Selection Flag for X"};
+  Configurable<double> cutYCandMax{"cutYCandMax", -1., "max. cand. rapidity"};
+  // Filter filterSelectCandidates = (aod::hf_selcandidate_x::isSelXToJpsiPiPi >= selectionFlagX);
+
+  void process(soa::Join<aod::HfCandX, aod::HfCandProng3MCRec> const& candidates,
+               soa::Join<aod::McParticles, aod::HfCandProng3MCGen> const& particlesMC)
+  {
+    // MC rec.
+    //Printf("MC Candidates: %d", candidates.size());
+    for (auto& candidate : candidates) {
+      if (!(candidate.hfflag() & 1 << XToJpsiPiPi)) {
+        continue;
+      }
+      if (cutYCandMax >= 0. && std::abs(YX(candidate)) > cutYCandMax) {
+        continue;
+      }
+      if (candidate.flagMCMatchRec() == 1 << XToJpsiPiPi) {
+        registry.fill(HIST("hPtRecSig"), candidate.pt());
+        registry.fill(HIST("hCPARecSig"), candidate.cpa());
+        registry.fill(HIST("hEtaRecSig"), candidate.eta());
+      } else {
+        registry.fill(HIST("hPtRecBg"), candidate.pt());
+        registry.fill(HIST("hCPARecBg"), candidate.cpa());
+        registry.fill(HIST("hEtaRecBg"), candidate.eta());
+      }
+    }
+    // MC gen.
+    //Printf("MC Particles: %d", particlesMC.size());
+    for (auto& particle : particlesMC) {
+      if (particle.flagMCMatchGen() == 1 << XToJpsiPiPi) {
+        if (cutYCandMax >= 0. && std::abs(RecoDecay::Y(array{particle.px(), particle.py(), particle.pz()}, RecoDecay::getMassPDG(particle.pdgCode()))) > cutYCandMax) {
+          //Printf("MC Gen.: Y rejection: %g", RecoDecay::Y(array{particle.px(), particle.py(), particle.pz()}, RecoDecay::getMassPDG(particle.pdgCode())));
           continue;
         }
-        for (auto track2 = track1 + 1; track2 != tracks.end(); ++track2) {
-          if (signTrack1 == track2.sign()) {
-            continue;
-          }
-          int indexTrack2 = track2.globalIndex();
-          if (signTrack1 > 0) {
-            if (indexTrack2 == index1jpsi) {
-              continue;
-            }
-          } else if (indexTrack2 == index0jpsi) {
-            continue;
-          }
-          registry.fill(HIST("hPtCand"), candidate.pt() + track1.pt() + track2.pt());
-        } // track2 loop (pion)
-      }   // track1 loop (pion)
-    }     // Jpsi loop
-  }       // process
-};        // struct
+        registry.fill(HIST("hPtGen"), particle.pt());
+        registry.fill(HIST("hEtaGen"), particle.eta());
+      }
+    }
+  } // process
+};  // struct
 
 WorkflowSpec defineDataProcessing(ConfigContext const& cfgc)
 {
   WorkflowSpec workflow{
     adaptAnalysisTask<TaskX>(cfgc, TaskName{"hf-task-x"})};
+  const bool doMC = cfgc.options().get<bool>("doMC");
+  if (doMC) {
+    workflow.push_back(adaptAnalysisTask<TaskXMC>(cfgc, TaskName{"hf-task-x-mc"}));
+  }
   return workflow;
 }
